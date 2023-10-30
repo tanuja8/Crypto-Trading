14fc6b0c9a138b4030c695a5c0a6187ea1785da1
Tanuja <tanujakhatun292@gmail.com>
2023-10-30 22:06:03 +0530

    project updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Homepages from './pages/Homepages';
+import Coinpage from './pages/Coinpage';
 
 function App() {
   return (
-    <div className="App">
-     hello world
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+      {/* <Route path="/" exact element={Homepages}/> */}
+      <Route path="/" exact element={Homepages}/>
+      <Route path="/coins/:id" element={Coinpage}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Coinpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Coinpage = () => {
+  return (
+    <div>
+        <h1>coin page</h1>
+      
+    </div>
+  )
+}
+
+export default Coinpage
