4eb51a2adf7c326ba029529c3bb0b48352f1fc28
Tanuja <tanujakhatun292@gmail.com>
2023-10-31 23:24:58 +0530

    add some style using material ui

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import './App.css';
 import Header from './components/Header';
 import Homepages from './pages/Homepages';
 import Coinpage from './pages/Coinpage';
+import { colors, makeStyles } from '@material-ui/core';
 
 function App() {
+  const useStyles = makeStyles(()=>({
+    App:{
+      backgroundcolor:"#14161a",
+      color:"white",
+      minHeight:"100vh",
+    },
+  }));
+  const classes =useStyles()
   return (
     <BrowserRouter>
+    <div className={classes.App}>
       <Header/>
       <Routes>
       {/* <Route path="/" exact element={Homepages}/> */}
-      <Route path="/" exact element={Homepages}/>
-      <Route path="/coins/:id" element={Coinpage}/>
+      <Route path="/" exact element={<Homepages/>}/>
+      <Route path="/coins/:id" element={<Coinpage/>}/>
       </Routes>
+      </div>
     </BrowserRouter>
   );
 }
