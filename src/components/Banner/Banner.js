e1889b8d3f0b75d2537170b76efc83804c761534
Tanuja <tanujakhatun292@gmail.com>
2023-11-21 00:58:38 +0530

    banner page created

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,13 +1,27 @@
-import { makeStyles } from '@material-ui/core'
+import { Container, makeStyles } from '@material-ui/core'
 import React from 'react'
 
 const useStyles=makeStyles(() =>({
-    
+   banner:{
+    background:"url(./banner.jpg)",
+   } ,
+   bannerContent:{
+    height :400,
+    display:"flex",
+    flexDirection:"column",
+    paddingTop:25,
+    justifyContent:"space-around",
+   },
 }))
 const Banner = () => {
+  const classes=useStyles()
   return (
     <div className={classes.banner}>
-      
+      <Container className={classes.bannerContent}>
+        <div className='classes.tagline'> */last line*/
+
+        </div>
+      </Container>
     </div>
   )
 }
