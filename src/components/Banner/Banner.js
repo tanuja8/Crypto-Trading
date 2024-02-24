215062436991ded0f808209f4df3d9d87aa31b40
Tanuja <tanujakhatun292@gmail.com>
2024-02-24 08:26:16 +0530

    created api Endpoints

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,47 +1,55 @@
-import { Container, Typography, makeStyles } from '@material-ui/core'
-import React from 'react'
+import { Container, Typography, makeStyles } from "@material-ui/core";
+import React from "react";
 
-const useStyles=makeStyles(() =>({
-   banner:{
-    background:"url(./banner.jpg)",
-    backgroundSize:"cover",
-   } ,
-   bannerContent:{
-    height :500,
-    display:"flex",
-    flexDirection:"column",
-    paddingTop:25,
-    justifyContent:"space-around",
-   },
-}))
+const useStyles = makeStyles(() => ({
+  banner: {
+    background: "url(./banner.jpg)",
+    backgroundSize: "cover",
+  },
+  bannerContent: {
+    height: 500,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  } /*last line*/,
+}));
 const Banner = () => {
-  const classes=useStyles()
+  const classes = useStyles();
   return (
     <div className={classes.banner}>
       <Container className={classes.bannerContent}>
-        <div className={classes.tagline}> 
-          <Typography 
-          variant="h2"
-          style={{
-            fontWeight:"bold",
-            marginBottom:15,
-            color:"white"
-          }}
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              color: "white",
+            }}
           >
             Crypto Hunter
           </Typography>
-          <Typography 
-          variant="subtitle2"
-          style={{
-            color:"darkgray",
-            textTransform:"capitalize"
-          }}>
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "darkgray",
+              textTransform: "capitalize",
+            }}
+          >
             Get all the Info regarding your favourite Crypto Currency
           </Typography>
         </div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
