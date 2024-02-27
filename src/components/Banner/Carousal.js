da5d331b0f68d72ae9adebda6b88dad999dda2e1
Tanuja <tanujakhatun292@gmail.com>
2024-02-28 00:14:46 +0530

    fetched the coin and design ui with carousal effect

--- src/components/Banner/Carousal.js
+++ src/components/Banner/Carousal.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { TrendingCoins } from '../../config/api';
 import { CryptoState } from '../../Cryptocontex';
+import AliceCarousel from 'react-alice-carousel';
+import { Link } from "react-router-dom";
 
 const useStyles=makeStyles((thems)=>({
   Carousal:{
@@ -25,9 +27,44 @@ const Carousal = () => {
     useEffect(()=>{
       fetchTrendingCoins();
         },[currency]);
+
+
+        const items=trending.map((coin) =>{
+          return(
+            <Link
+            className={classes.CarousalItem} to={`/coins/${coin.id}`}>
+              <img
+              src={coin?.image}
+              alt={coin.name}
+              height="80"
+              style={{marginBottom:10}}
+              />
+
+              </Link>
+            
+          )
+        })
+
+        const responsive = {
+          0: {
+            items: 2,
+          },
+          512: {
+            items: 4,
+          },
+        };
   return (
     <div className={classes.Carousal}>
-      Carousal
+     <AliceCarousel
+     mouseTracking
+     infinite
+     autoPlayInterval={1000}
+     animationDuration={1500}
+     disableDotsControls
+     responsive={responsive}
+     autoPlay
+     items={items}
+     />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Cryptocontex from './Cryptocontex';
-
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
