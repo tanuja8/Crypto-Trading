6ac87c72895f935b5d9abca136fa7603e8e226a4
Tanuja <tanujakhatun292@gmail.com>
2024-02-29 11:24:11 +0530

    coinsTable updated

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -2,11 +2,25 @@ import React, { useEffect, useState } from "react";
 import { CoinList } from "../config/api";
 import axios from "axios";
 import { CryptoState } from "../Cryptocontex";
-import { Container, CssBaseline, ThemeProvider, Typography, createTheme } from "@material-ui/core";
+import {
+  Container,
+  CssBaseline,
+  LinearProgress,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@material-ui/core";
 
 const CoinsTable = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
 
   const { currency } = CryptoState();
 
@@ -21,23 +35,57 @@ const CoinsTable = () => {
     fetchCoins();
   }, [currency]);
 
-    const darkTheme=createTheme({
-      palette:{
-        primary:{
-          main:"#fff",
-        },
-        type:"dark",
-      }
-    })
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
 
-  return(
-<ThemeProvider theme={darkTheme}>
-<CssBaseline/>
-    <Container style={{textAlign:"center"}}>
-      <Typography variant="h4" style={{margin:18}}>Cryptocurrency Price by Market Cap</Typography>
-    </Container>
-</ThemeProvider>
-  ) 
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container style={{ textAlign: "center" }}>
+        <Typography variant="h4" style={{ margin: 18 }}>
+          Cryptocurrency Price by Market Cap
+        </Typography>
+        <TextField
+          label="Search For a Crypto Currency..."
+          variant="outlined"
+          style={{ margin: 20, width: "100%" }}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </Container>
+      <TableContainer>
+      {loading ? (
+            <LinearProgress style={{ backgroundColor: "gold" }} />
+          ) : (
+            <Table>
+              <TableHead style={{backgroundColor:"#EEBC1D"}}>
+                <TableRow>
+                {["Coin", "Price", "24h Change", "Market Cap"].map((head) => (
+                    <TableCell
+                      style={{
+                        color: "black",
+                        fontWeight: "700",
+                        fontFamily: "Montserrat",
+                      }}
+                      key={head}
+                      align={head === "Coin" ? "" : "right"}
+                    >
+                      {head}
+                    </TableCell>
+                  ))}
+                </TableRow>
+
+              </TableHead>
+            </Table>
+        )}
+      </TableContainer>
+    </ThemeProvider>
+  );
 };
 
 export default CoinsTable;
