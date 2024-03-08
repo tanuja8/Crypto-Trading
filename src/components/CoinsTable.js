1a9dbb7b2ecd2c48b2a5c29101ec2818ccd78fd7
Tanuja <tanujakhatun292@gmail.com>
2024-03-08 22:36:21 +0530

    makestyle issue problem fixed

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Pagination from "@material-ui/lab/Pagination";
 import { CoinList } from "../config/api";
 import axios from "axios";
 import { CryptoState } from "../Cryptocontex";
+import { useNavigate } from "react-router-dom";
+
 import {
   Container,
   CssBaseline,
   LinearProgress,
   Table,
+  TableBody,
   TableCell,
   TableContainer,
   TableHead,
@@ -17,6 +22,7 @@ import {
   createTheme,
 } from "@material-ui/core";
 
+
 const CoinsTable = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -24,16 +30,23 @@ const CoinsTable = () => {
 
   const { currency } = CryptoState();
 
-  const fetchCoins = async () => {
-    setLoading(true);
-    const { data } = await axios.get(CoinList(currency));
-    setCoins(data);
-    setLoading(false);
-  };
-  console.log(coins);
-  useEffect(() => {
-    fetchCoins();
-  }, [currency]);
+  // const useStyles = makeStyles({
+  //   row: {
+  //     backgroundColor: "#16171a",
+  //     cursor: "pointer",
+  //     "&:hover": {
+  //       backgroundColor: "#131111",
+  //     },
+  //     fontFamily: "Montserrat",
+  //   },
+  //   Pagination: {
+  //     "& .MuiPaginationItem-root": {
+  //       color: "gold",
+  //     },
+  //   },
+  // });
+  // const classes = useStyles();
+  const navigate = useNavigate();
 
   const darkTheme = createTheme({
     palette: {
@@ -43,6 +56,34 @@ const CoinsTable = () => {
       type: "dark",
     },
   });
+
+  const fetchCoins = async () => {
+    setLoading(true);
+    const { data } = await axios.get(CoinList(currency));
+    setCoins(data);
+    setLoading(false);
+  };
+  console.log(coins);
+
+  useEffect(() => {
+    fetchCoins();
+  }, [currency]);
+
+  // const handleSearch = () => {
+  //   return coins.filter(
+  //     (coin) =>
+  //       coin.name.toLowerCase().includes(search) ||
+  //       coin.symbol.toLowerCase().includes(search)
+  //   );
+  // };
+
+  const handleSearch = () => {
+    return coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(search.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(search.toLowerCase())
+    );
+  };
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -59,29 +100,49 @@ const CoinsTable = () => {
         />
       </Container>
       <TableContainer>
-      {loading ? (
-            <LinearProgress style={{ backgroundColor: "gold" }} />
-          ) : (
-            <Table>
-              <TableHead style={{backgroundColor:"#EEBC1D"}}>
-                <TableRow>
+        {loading ? (
+          <LinearProgress style={{ backgroundColor: "gold" }} />
+        ) : (
+          <Table>
+            <TableHead style={{ backgroundColor: "#EEBC1D" }}>
+              <TableRow>
                 {["Coin", "Price", "24h Change", "Market Cap"].map((head) => (
+                  <TableCell
+                    style={{
+                      color: "black",
+                      fontWeight: "700",
+                      fontFamily: "Montserrat",
+                    }}
+                    key={head}
+                    align={head === "Coin" ? "" : "right"}
+                  >
+                    {head}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {handleSearch().map((row) => {
+                const profit = row.price_change_percentage_24h > 0;
+                return (
+                  <TableRow
+                    onClick={() => navigate.push(`/coins/${row.id}`)}
+                    // className={classes.row}
+                    key={row.name}
+                  >
                     <TableCell
+                      component="th"
+                      scope="row"
                       style={{
-                        color: "black",
-                        fontWeight: "700",
-                        fontFamily: "Montserrat",
+                        display: "flex",
+                        gap: 15,
                       }}
-                      key={head}
-                      align={head === "Coin" ? "" : "right"}
-                    >
-                      {head}
-                    </TableCell>
-                  ))}
-                </TableRow>
-
-              </TableHead>
-            </Table>
+                    ></TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
         )}
       </TableContainer>
     </ThemeProvider>
