694c40eddabc1ba222fc74682d2083bbfffe02f6
Tanuja <tanujakhatun292@gmail.com>
2024-03-14 23:50:23 +0530

    update the coinpage component

--- src/pages/Coinpage.js
+++ src/pages/Coinpage.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { SingleCoin } from '../config/api'
+import {CryptoState} from "../Cryptocontex"
+import axios from 'axios'
 
 const Coinpage = () => {
+  const {id}=useParams()
+  const [coin,setCoin]=useState()
+  const {currency,symbol}=CryptoState();
+  const fetchCoin= async()=>{
+    const {data}= await axios.get(SingleCoin(id));
+    setCoin(data);
+    };
+    console.log(coin)
+    useEffect(()=>{
+      fetchCoin()
+    },[]);
+
+
   return (
     <div>
         <h1>coin page</h1>
